e3b2cef42eafd875c66cdb6c874615bb6fc429a1
Dans <drocks2000@@gmail.com>
2019-03-17 19:53:25 +0100

    [Auth] Modificar tabla user para implementar sistema de roles básico

---
+++ webapp/database/migrations/2019_03_17_181337_add_role_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\Interfaces\IUser;
+
+class AddRoleToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role', 10)
+                ->default(IUser::ROLE_USER)
+                ->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
+    }
+}
