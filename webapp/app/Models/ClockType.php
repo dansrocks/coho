10df38ff6143900e672bd194294f38418b2887ad
Dans <drocks2000@@gmail.com>
2019-03-17 12:45:26 +0100

    [ClockType] Crear tabla y modelo para Tipos de registros de tiempo

---
+++ webapp/app/Models/ClockType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ClockType extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * @var string
+     */
+    protected $table = 'clock_types';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'key',
+        'description'
+    ];
+}
