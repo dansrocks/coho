a8685026b874514ad0580cfbbd36d5a3e3643389
Dans <drocks2000@@gmail.com>
2019-03-18 23:24:24 +0100

    [Usuarios] Eliminar acción show porque no se usa

--- webapp/app/Http/Controllers/UserManagementController.php
+++ webapp/app/Http/Controllers/UserManagementController.php
@@ -68,17 +68,6 @@ class UserManagementController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
