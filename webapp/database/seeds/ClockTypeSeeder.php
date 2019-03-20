33672a3224ea18fed61fb9fb3be0e166408b0d3b
Dans <drocks2000@@gmail.com>
2019-03-20 23:29:28 +0100

    [Seeder] Seeder para tipos de registros de tiempo

---
+++ webapp/database/seeds/ClockTypeSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Interfaces\ITimeClock;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Class ClockTypeSeeder
+ */
+class ClockTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach ($this->getClockTypesData() as $clockTypesData) {
+            DB::table('clock_types')->insert([
+                'name' => $clockTypesData['name'],
+                'description' => $clockTypesData['description'],
+            ]);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    private function getClockTypesData()
+    {
+        return [
+            [
+                'name' => ITimeClock::TYPE_OUT_OFF_OFFICE,
+                'description' => 'Fuera de la oficina',
+            ],
+            [
+                'name' => ITimeClock::TYPE_WORKING,
+                'description' => 'Horario laboral',
+            ],
+            [
+                'name' => ITimeClock::TYPE_LUNCH,
+                'description' => 'Hora de comida',
+            ],
+            [
+                'name' => ITimeClock::TYPE_BREAK,
+                'description' => 'Descanso',
+            ],
+        ];
+    }
+}
