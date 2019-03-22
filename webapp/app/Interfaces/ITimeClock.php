b6be01520e5ca1673f4331da75546aeb2ffc0be5
Dans <drocks2000@@gmail.com>
2019-03-22 20:18:49 +0100

    Actualización de la interfaz ITimeClock

---
+++ webapp/app/Interfaces/ITimeClock.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Interfaces;
+
+use App\User;
+use Carbon\Carbon;
+
+/**
+ * Interface ITimeClock
+ *
+ * @package App\Interfaces
+ */
+interface ITimeClock
+{
+    const TYPE_WORKING = 'working';
+    const TYPE_LUNCH = 'lunch';
+    const TYPE_BREAK = 'break';
+    const TYPE_OUT_OFF_OFFICE = 'outwork';
+
+    /**
+     * @param User $user
+     *
+     * @return ITimeClock
+     */
+    public function setUser(User $user) : ITimeClock;
+
+    /**
+     * @return string
+     */
+    public function getType() : string;
+
+    /**
+     * @param string type
+     *
+     * @return ITimeClock
+     */
+    public function setType(string $type) : ITimeClock;
+
+    /**
+     * @return Carbon
+     */
+    public function getDate() : Carbon;
+
+    /**
+     * @param Carbon $date
+     *
+     * @return ITimeClock
+     */
+    public function setDate(Carbon $date) : ITimeClock;
+
+    /**
+     * @param Carbon $datetime
+     *
+     * @return ITimeClock
+     */
+    public function setClockInTime(Carbon $datetime) : ITimeClock;
+
+    /**
+     * @param Carbon $datetime
+     *
+     * @return ITimeClock
+     */
+    public function setClockOutTime(Carbon $datetime) : ITimeClock;
+
+    /**
+     * @return bool
+     */
+    public function onClockIn() : bool;
+
+    /**
+     * @return bool
+     */
+    public function onClockOut() : bool;
+}
