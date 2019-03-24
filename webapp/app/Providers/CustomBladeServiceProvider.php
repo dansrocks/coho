d7360aac212143f9150bca12538db3237314cba8
Dans <drocks2000@@gmail.com>
2019-03-24 20:19:36 +0100

    Mejora en directivas personalizadas Blade para mostrar fechas y periodos

--- webapp/app/Providers/CustomBladeServiceProvider.php
+++ webapp/app/Providers/CustomBladeServiceProvider.php
@@ -2,12 +2,7 @@
 
 namespace App\Providers;
 
-use App\User;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\ServiceProvider;
 
 /**
@@ -33,42 +28,16 @@ class CustomBladeServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::directive('date', function (Carbon $date) {
-            return "<?php echo $date->format('d/m/Y'); ?>";
+        Blade::directive('date', function ($date) {
+            return "<?php echo ({$date})->format('d/m/Y'); ?>";
         });
 
         Blade::directive('time', function ($expression) {
-            return "<?php echo ($expression)->format('H:i'); ?>";
+            return "<?php echo ({$expression})->format('H:i'); ?>";
         });
 
-        Blade::directive('duration', function ($timeInSeconds, $withSeconds = false) {
-
-            $seconds = $timeInSeconds % 60;
-
-            $time = floor($timeInSeconds / 60);
-            $minutes = ($time % 60);
-
-            $time = floor($time / 60);
-            $hours = floor($time / 60);
-
-            $timeAsText = '';
-            if ($hours > 0 ) {
-                $timeAsText .= sprintf("%d h", $hours);
-            }
-
-            if ($minutes > 0 ) {
-                $timeAsText .= sprintf("%d m", $hours);
-            }
-
-            if ($withSeconds && $seconds > 0 ) {
-                $timeAsText .= sprintf("%d s", $hours);
-            }
-
-            if (empty($timeAsText)) {
-                $timeAsText = '0 s';
-            }
-
-            return "<?php echo ($timeAsText); ?>";
+        Blade::directive('duration', function ($timeInSeconds) {
+            return "<?php echo TimeDurationHelper::showDurationHumanFriendly({$timeInSeconds}, false); ?>";
         });
     }
 }
